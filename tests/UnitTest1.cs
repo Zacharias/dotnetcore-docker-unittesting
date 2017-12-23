d8bd99374e2a2aa730c98a8611889b6447625dd2
Zacharias Stankiewicz <zacharias.stankiewicz@gmail.com>
2017-12-23 15:48:25 -0500

    added reference to webapi and testing service's string

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using api;
 
 namespace tests
 {
     public class UnitTest1
     {
+
         [Fact]
         public void Test1()
         {
-
+            var testedservice = new api.Services.MyCoolService();
+            string mystring = testedservice.GiveCoolString();
+            
+            Assert.InRange(mystring.Length, 1, 100);
+            Assert.Contains("Brah", mystring);
         }
     }
 }
